f199fa579c8a1878f40d34a306f0779e37785775
David Dionisio <ddionisio@renegadeware.com>
2020-10-23 12:15:18 -0400

    added TextVersionTextMeshPro.cs

---
+++ Scripts/TextVersionTextMeshPro.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using TMPro;
+
+namespace M8.TextMeshPro {
+    [AddComponentMenu("M8/TextMeshPro/Version")]
+    public class TextVersionTextMeshPro : MonoBehaviour {
+        public TMP_Text target;
+
+        public void Apply() {
+            if(!target)
+                target = GetComponent<TMP_Text>();
+
+            if(target)
+                target.text = Application.version;
+        }
+
+        void Awake() {
+            Apply();
+        }
+    }
+}
